4491f555b56b309d0b98c0eea93fcf8bc0272c46
Nir Benita <benitanir@gmail.com>
2017-11-12 01:45:02 -0600

    add typing to TodoList

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
+import * as React from 'react';
 
-export const TodoList = () => {<div>
-    Yo
-</div>}
+export const TodoList: React.SFC<{}> = () => {
+  <div>Yo</div>
+}
